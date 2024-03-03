fa47b399f648b47944ff1613169af8b9f9c10f62
khushi <khushi@gmail.com>
2024-03-03 18:02:33 +0530

    'token'

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer 
 from .models import User 
-import datetime 
+import jwt, datetime
 
 class RegisterView(APIView):
     def post(self,request):
@@ -12,7 +12,29 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    
+
+class LoginView(APIView):
+    def post(self,request):
+        email=request.data['email']
+        password=request.data['password']
+        user=User.objects.filter(email=email).first() 
+        if user is None:
+            raise AuthenticationFailed('user not found')
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password')
+        payload={
+            'id': user.id,
+            'exp': datetime.datetime.utcnow()+datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        } 
+        token = jwt.encode(payload,'secret',algorithm='HS256')
+        response= Response()
+        response.set_cookie(key='jwt',value=token,httponly=True)
+        response.data={
+            'jwt': token
+        }
+        return response
+            
 
 
 
