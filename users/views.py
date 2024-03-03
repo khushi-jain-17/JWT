b0d2e29cc45ac2e796f69ed266a3b48427fd2160
khushi <khushi@gmail.com>
2024-03-03 19:16:02 +0530

    'user'

--- users/views.py
+++ users/views.py
@@ -34,7 +34,39 @@ class LoginView(APIView):
             'jwt': token
         }
         return response
-            
 
+class UserView(APIView):
+    def get(self,request):
+        token=request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+        try:
+            payload=jwt.decode(token,'secret',algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated')
+        user=User.objects.filter(id=payload['id']).first()
+        serializer=UserSerializer(user)
+        return Response(serializer.data)
+            
+            
+# class UserView(APIView):
+#     def get(self, request):
+#         token = request.COOKIES.get('jwt')
+#         if not token:
+#             raise AuthenticationFailed('Unauthenticated')
+
+#         try:
+#             payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+#         except jwt.ExpiredSignatureError:
+#             raise AuthenticationFailed('Token has expired')
+#         except jwt.DecodeError:
+#             raise AuthenticationFailed('Token is invalid')
+
+#         user = User.objects.filter(id=payload['id']).first()
+#         if not user:
+#             raise AuthenticationFailed('User not found')
+
+#         serializer = UserSerializer(user)
+#         return Response(serializer.data)
 
 
